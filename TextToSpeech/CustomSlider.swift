3edaf30f7579e2db993917f7280bd5999f1cc85c
Nitin Bhatia <nbnitin.bhatia@gmail.com>
2020-07-23 19:45:55 +0530

    first commit

---
+++ TextToSpeech/CustomSlider.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSlider.swift
+//  SpeechDemo
+//
+//  Created by Gabriel Theodoropoulos on 10/2/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomSlider: UISlider {
+
+    var sliderIdentifier: Int! = 0
+    
+   
+
+}
